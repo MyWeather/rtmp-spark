1c42022156944bcab5393aea5bbd0e78ceadd95f
Tyler Youschak <tyleryouschak@gmail.com>
2015-09-29 16:24:59 -0400

    Routes and views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,19 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->welcome();
-});
+$app->get('/', 'HomeController@index');
+
+/* Auth Block */
+$app->get('/auth/register', ['as' => 'auth_register', 'uses' => 'AccountController@getRegister']);
+$app->post('/auth/register', 'AccountController@postRegister');
+$app->get('/auth/login', ['as' => 'auth_login', 'uses' => 'AuthController@getLogin');
+$app->post('/auth/login', 'AuthController@postLogin');
+$app->get('/auth/password/reset', ['as' => 'auth_password_reset', 'uses' => 'AuthController@getLogin');
+$app->post('/auth/password/reset', 'AuthController@postLogin');
+
+
+/* Account Block */
+$app->get('/account/home', 'AccountController@home');
+
+/* Bradcast Block */
+$app->get('/broadcast/auth', ['as' => 'broadcast_auth', 'uses' => 'BroadcastController@auth']]);
